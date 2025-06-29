f4ffcc8be63654df1f62c18ab6cf4db757342f83
GitHub Action <action@github.com>
2025-06-29 12:26:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/brandon3343/bettersolarized/MyBundle.kt
+++ src/main/kotlin/com/github/brandon3343/bettersolarized/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.brandon3343.bettersolarized
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/brandon3343/bettersolarized/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/brandon3343/bettersolarized/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.brandon3343.bettersolarized.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
